a68e9734380725fe99ad8f98404216b760138331
SriKumar982 <ksri33138@gmail.com>
2025-12-19 15:49:59 +0530

    login done

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -5,7 +5,8 @@
 
 // src/config/api.js
 
-export const API_BASE = "http://192.168.0.8:5000";
+// export const API_BASE = "http://192.168.0.8:5000";
+export const API_BASE = "http://192.168.0.18:8000/api";
 export const LIVEKIT_URL = "wss://teststream-lepxfkhc.livekit.cloud";
 export const API_BASE_URL ="https://api.arinnovate.io/api";
 
