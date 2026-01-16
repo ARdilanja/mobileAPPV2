17c238f5ceb4f31f68fd57e7f408ae2f9b7b1384
preethi-gopal <preethi@arinnovate.io>
2026-01-16 13:44:27 +0530

    add circular scroll in practice item

--- src/screens/SpeakingInPracticeScreen.jsx
+++ src/screens/SpeakingInPracticeScreen.jsx
@@ -1,5 +1,5 @@
 import { useFocusEffect } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import {
   View,
   Text,
@@ -7,7 +7,9 @@ import {
   Dimensions,
   Pressable,
   ScrollView,
-  StatusBar, Image
+  StatusBar,
+  Image,
+  Platform,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { Fonts } from '../constants/fonts';
@@ -15,8 +17,29 @@ import DayCompleScrolComponent from '../components/invitedInterview/DayCompleScr
 
 const { width } = Dimensions.get('window');
 const scale = width / 390;
+const STATUS_BAR_HEIGHT =
+  Platform.OS === 'android' ? StatusBar.currentHeight || 0 : 0;
+
+const ITEM_WIDTH = 130 * scale;
+
+const PRACTICE_OPTIONS = [
+  'Promotion &\nAppraisal',
+  'Speak in\nMeetings',
+  'Saying\nNo',
+];
+
+
+const LOOPED_OPTIONS = [
+  ...PRACTICE_OPTIONS,
+  ...PRACTICE_OPTIONS,
+  ...PRACTICE_OPTIONS,
+];
 
 export default function SpeakingInPracticeScreen() {
+  const scrollRef = useRef(null);
+  const middleIndex = PRACTICE_OPTIONS.length;
+  const [activePracticeIndex, setActivePracticeIndex] = useState(1);
+
   useFocusEffect(
     useCallback(() => {
       StatusBar.setBarStyle('dark-content');
@@ -24,182 +47,216 @@ export default function SpeakingInPracticeScreen() {
       StatusBar.setTranslucent(true);
     }, []),
   );
+  React.useEffect(() => {
+    setTimeout(() => {
+      scrollRef.current?.scrollTo({
+        x: middleIndex * ITEM_WIDTH,
+        animated: false,
+      });
+    }, 50);
+  }, []);
+
+  const handleScroll = (event) => {
+    const x = event.nativeEvent.contentOffset.x;
+    const index = Math.round(x / ITEM_WIDTH);
+    setActivePracticeIndex(index);
+  };
+  const handleMomentumScrollEnd = (event) => {
+    const x = event.nativeEvent.contentOffset.x;
+    const rawIndex = Math.round(x / ITEM_WIDTH);
+    const centerX = x + width / 2;
+    const realIndex =
+      ((rawIndex % PRACTICE_OPTIONS.length) + PRACTICE_OPTIONS.length) %
+      PRACTICE_OPTIONS.length;
+
+    setActivePracticeIndex(realIndex);
+
+    // Jump back to middle silently
+    if (rawIndex <= PRACTICE_OPTIONS.length ||
+      rawIndex >= PRACTICE_OPTIONS.length * 2) {
+      scrollRef.current?.scrollTo({
+        x: (realIndex + PRACTICE_OPTIONS.length) * ITEM_WIDTH,
+        animated: false,
+      });
+    }
+  };
 
   return (
-    <LinearGradient
-      colors={[
-        '#EBE6FF',
-        'rgba(235, 230, 255, 0)',
-        '#FFFFFF',
-      ]}
-      locations={[0, 0.5, 1]}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 0, y: 1 }}
-      style={{ flex: 1 }}
-    >
-      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-        <Text style={styles.todayText}>Today’s session</Text>
-        <Text style={styles.title}>Speaking up in meetings</Text>
-        <Text style={styles.week}>Week 1</Text>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      {/* TOP GRADIENT */}
+      <View style={styles.topGradientWrapper}>
+        <LinearGradient
+          colors={['#FFFFFF', '#EBE6FF']}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+          style={styles.topGradient}
+        >
+          <View style={styles.topContent}>
+            <Text style={styles.todayText}>Today’s session</Text>
+            <Text style={styles.title}>Speak first in today’s standup</Text>
+            <Text style={styles.week}>Week 1</Text>
 
-        {/* ✅ Reused Component */}
-        <DayCompleScrolComponent style={styles.dayScrollpart} totalDays={9} activeDay={4} />
+            <DayCompleScrolComponent
+              style={styles.dayScrollpart}
+              totalDays={9}
+              activeDay={4}
+            />
 
-        <Pressable style={styles.primaryBtn}>
-          <Text style={styles.primaryBtnText}>Start today’s session</Text>
-        </Pressable>
+            <Pressable style={styles.primaryBtn}>
+              <Text style={styles.primaryBtnText}>Start today’s session</Text>
+            </Pressable>
 
-        <Pressable style={styles.secondaryBtn}>
-          <Text style={styles.secondaryBtnText}>View full plan</Text>
-        </Pressable>
+            <Pressable style={styles.secondaryBtn}>
+              <Text style={styles.secondaryBtnText}>View full plan</Text>
+            </Pressable>
+          </View>
+        </LinearGradient>
+      </View>
 
+      <View style={{ paddingHorizontal: 16 * scale }}>
         {/* SESSION CARD */}
         <View style={styles.sessionCard}>
-          {/* LEFT CONTENT */}
-          <View>
-            <Text style={styles.sessionLabel}>Session 1</Text>
-            <Text style={styles.sessionTitle}>Speak in Meetings</Text>
+          <View style={styles.sessionCardRow}>
+            <Text style={styles.sessGreenLabel}>Last session feedback</Text>
 
-            <View style={styles.sessionMeta}>
-              <View style={styles.dateChip}>
-                <Image
-                  source={require('../assets/icons/calendar-day.png')}
-                  style={styles.metaIcon}
-                />
-                <Text style={styles.metaText}>Jan 07, 2025</Text>
-              </View>
-
-              <View style={styles.timeChip}>
-                <Image
-                  source={require('../assets/icons/clock-three.png')}
-                  style={styles.metaIcon}
-                />
-                <Text style={styles.metaText}>7 mins</Text>
-              </View>
+            <View style={styles.sessGreenDateChip}>
+              <Text style={styles.sessGreenDate}>Jan 20, 2026</Text>
             </View>
-
           </View>
 
-          {/* RIGHT SIDE */}
-          <View style={styles.rightWrap}>
-            {/* Arrow Icon */}
+          <Text style={styles.sessGreenTitle}>
+            Speak first in today’s standup
+          </Text>
+
+          <Pressable style={styles.fedbackBtn}>
+            <Text style={styles.fedbackBtnText}>View feedback</Text>
             <Image
               source={require('../assets/images/angle-small.png')}
-              style={styles.arrowIcon}
+              style={styles.whitearrowIcon}
             />
-            {/* Greeting Image */}
-            <View style={styles.greetingPart}>
-              <View style={styles.pointsRow}>
-                <Text style={styles.streakNumber}>23</Text>
-                <Image
-                  source={require('../assets/icons/greatings.png')}
-                  style={styles.greetingImg}
-                />
-              </View>
-
-              <Text style={styles.streakText}>Points Earned</Text>
-            </View>
-
-
-          </View>
+          </Pressable>
         </View>
 
-
+        {/* PRACTICE SECTION */}
         <View style={styles.lineWrapper}>
-          <Text style={styles.startPracticeTitle}>Start an Ad-hoc Practice</Text>
+          <Text style={styles.startPracticeTitle}>
+            Start an Ad-hoc Practice
+          </Text>
 
-          <View style={styles.practiceRow}>
-            {/* Left */}
-            <View style={styles.practiceItem}>
-              <Text style={styles.practiceTextMuted}>Promotion & Appraisal</Text>
-            </View>
+          {/* CENTER FIXED INDICATOR */}
+          <View style={styles.carouselWrapper}>
+            {/* CENTER INDICATOR (RESPONSIVE) */}
 
-            {/* Center (Active) */}
-            <View style={styles.practiceCenterItem}>
-              <Text style={styles.practiceTextActive}>Speak in Meetings</Text>
 
+            {/* HORIZONTAL SCROLL */}
+            <ScrollView
+              ref={scrollRef}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              snapToInterval={ITEM_WIDTH}
+              decelerationRate="fast"
+              onMomentumScrollEnd={handleMomentumScrollEnd}
+              contentContainerStyle={styles.practiceRow}
+            >
+              {/* LEFT SPACER */}
+              <View style={{ width: width / 2 - ITEM_WIDTH / 2 }} />
+
+              {LOOPED_OPTIONS.map((item, index) => {
+                const realIndex = index % PRACTICE_OPTIONS.length;
+                const isActive = realIndex === activePracticeIndex;
+
+                return (
+                  <View key={`${item}-${index}`} style={styles.practiceItem}>
+                    <Text
+                      numberOfLines={2}
+                      style={
+                        isActive
+                          ? styles.practiceTextActive
+                          : styles.practiceTextMuted
+                      }
+                    >
+                      {item}
+                    </Text>
+                  </View>
+                );
+              })}
+
+              {/* RIGHT SPACER */}
+              <View style={{ width: width / 2 - ITEM_WIDTH / 2 }} />
+            </ScrollView>
+
+            <View style={styles.centerIndicatorContainer} pointerEvents="none">
               <LinearGradient
-                colors={['#4A2AC9', 'rgba(245,245,245,0)',]}
-
-                start={{ x: 0, y: 0 }}
+                colors={['#235DFF', '#F5F5F500']}
+                start={{ x: 1, y: 0 }}
                 end={{ x: 0, y: 1 }}
                 style={styles.verticalLine}
               />
             </View>
-
-            {/* Right */}
-            <View style={styles.practiceItem}>
-              <Text style={styles.practiceTextMuted}>Saying No</Text>
-            </View>
           </View>
-        </View>
 
+
+        </View>
 
         <Pressable style={styles.practiceBtn}>
           <Text style={styles.practiceBtnText}>Start practice</Text>
         </Pressable>
-      </ScrollView>
-    </LinearGradient>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 16 * scale,
-    marginTop: 40 * scale,
+  container: { flex: 1, backgroundColor: '#FFFFFF' },
 
+  topGradientWrapper: {
+    height: 362 * scale + STATUS_BAR_HEIGHT,
+    marginTop: -STATUS_BAR_HEIGHT,
+    borderBottomLeftRadius: 16 * scale,
+    borderBottomRightRadius: 16 * scale,
+    overflow: 'hidden',
+  },
+
+  topGradient: { flex: 1 },
+
+  topContent: {
+    marginTop: STATUS_BAR_HEIGHT + 40 * scale,
+    paddingHorizontal: 16 * scale,
   },
 
   todayText: {
     marginTop: 24 * scale,
     color: '#4A2AC9',
     fontSize: 14 * scale,
-    lineHeight: 20 * scale,
-    fontFamily: Fonts.Regular
+    fontFamily: Fonts.Regular,
   },
 
   title: {
     marginTop: 8 * scale,
-    color: '#000',
     fontSize: 24 * scale,
-    lineHeight: 32 * scale,
-    fontFamily: Fonts.Medium
+    fontFamily: Fonts.Medium,
   },
 
   week: {
     marginTop: 16 * scale,
-    color: '#2A2A2A',
     fontSize: 14 * scale,
-    lineHeight: 20 * scale,
-    fontFamily: Fonts.Regular
+    fontFamily: Fonts.Regular,
   },
 
-  /* Day Scroll */
-  dayScrollpart: {
-    marginTop: 8 * scale,
-    paddingRight: 16 * scale,
-  },
+  dayScrollpart: { marginTop: 8 * scale },
 
-
-  /* Buttons */
   primaryBtn: {
     marginTop: 24 * scale,
     height: 40 * scale,
     borderRadius: 8 * scale,
     backgroundColor: '#4A2AC9',
-    borderColor: '#4A2AC9',
-
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: 16 * scale,
   },
 
   primaryBtnText: {
-    color: '#FFF3EA',
+    color: '#FFF',
     fontFamily: Fonts.Medium,
-    lineHeight: 24 * scale,
-    paddingVertical: 8 * scale
   },
 
   secondaryBtn: {
@@ -210,174 +267,97 @@ const styles = StyleSheet.create({
     borderColor: '#4A2AC9',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: 16 * scale,
   },
 
   secondaryBtnText: {
     color: '#4A2AC9',
     fontFamily: Fonts.Medium,
-    lineHeight: 24 * scale,
-    paddingVertical: 8 * scale
   },
 
-  /* Session Card */
   sessionCard: {
     marginTop: 24 * scale,
-    height: 120 * scale,
     borderRadius: 16 * scale,
-    backgroundColor: '#E1D9FF',
-    padding: 16 * scale,
+    backgroundColor: '#E8E2FF',
+    padding: 12 * scale,
+  },
+
+  sessionCardRow: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
   },
 
-  sessionLabel: {
+  sessGreenLabel: {
     fontSize: 14 * scale,
     fontFamily: Fonts.Regular,
-    lineHeight: 20 * scale,
-    color: '#6B5CFF',
+    color: '#4A2AC9',
   },
 
-  sessionTitle: {
+  sessGreenDateChip: {
+    borderRadius: 8 * scale,
+    paddingVertical: 4 * scale,
+    paddingHorizontal: 8 * scale,
+    backgroundColor: '#D7CCFF',
+  },
+
+  sessGreenDate: {
+    fontSize: 14 * scale,
+    fontFamily: Fonts.Regular,
+  },
+
+  sessGreenTitle: {
     fontSize: 18 * scale,
     fontFamily: Fonts.Medium,
-    lineHeight: 24 * scale,
     marginTop: 12 * scale,
   },
 
-  sessionMeta: {
+  fedbackBtn: {
+    marginTop: 16 * scale,
+    width: 169 * scale,
+    height: 40 * scale,
+    borderRadius: 8 * scale,
+    backgroundColor: '#4A2AC9',
     flexDirection: 'row',
-    gap: 8 * scale,
-    marginTop: 12 * scale,
-  },
-
-  metaIcon: {
-    width: 12 * scale,
-    height: 12 * scale,
-    resizeMode: 'contain',
-  },
-
-  dateChip: {
-    height: 28 * scale,
-    width: 120 * scale,
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 8 * scale,
-    paddingVertical: 4 * scale,
-    gap: 8 * scale,
-    borderRadius: 4 * scale,
-    backgroundColor: '#EBE6FF',
-  },
-
-  timeChip: {
-    height: 28 * scale,
-    width: 79 * scale,
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 8 * scale,
-    paddingVertical: 4 * scale,
-    gap: 8 * scale,
-    borderRadius: 4 * scale,
-    backgroundColor: '#EBE6FF',
-  },
-
-
-  pointsWrap: {
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-  },
-
-  points: {
-    fontSize: 22 * scale,
-    fontWeight: '700',
-  },
-
-  pointsLabel: {
-    fontSize: 12 * scale,
-    color: '#8E8E93',
-    marginTop: 4 * scale,
-  },
-  rightWrap: {
     justifyContent: 'space-between',
-    alignItems: 'flex-end',
-    height: '100%',
-  },
-
-  greetingImg: {
-    width: 24 * scale,
-    height: 32 * scale,
-    resizeMode: 'contain',
-  },
-  greetingPart: {
-    alignItems: 'flex-end',
-    width: 88 * scale,
-    height: 68 * scale,
-    marginRight: 24 * scale,
-  },
-
-  pointsRow: {
-    flexDirection: 'row',
     alignItems: 'center',
-    gap: 6 * scale,
+    paddingHorizontal: 12 * scale,
   },
 
-  streakNumber: {
-    fontSize: 32 * scale,
+  fedbackBtnText: {
+    color: '#FFF',
     fontFamily: Fonts.Medium,
-    lineHeight: 48 * scale,
-    color: '#000',
   },
 
-  streakText: {
-    marginTop: 2 * scale,
-    fontSize: 14 * scale,
-    fontFamily: Fonts.Regular,
-    lineHeight: 20 * scale,
-
-    color: '#000',
-  },
-  arrowIcon: {
+  whitearrowIcon: {
+    tintColor: '#FFF',
     width: 20 * scale,
     height: 20 * scale,
-    resizeMode: 'contain',
-    // marginTop: 8 * scale,
   },
 
-  lineWrapper: {
-    marginVertical: 26 * scale,
-  },
+  lineWrapper: { marginVertical: 26 * scale },
 
   startPracticeTitle: {
     fontSize: 18 * scale,
     fontFamily: Fonts.Medium,
-    color: '#000',
     marginBottom: 28 * scale,
-    lineHeight: 24 * scale,
-    textAlign: "left"
   },
 
   practiceRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-    width: '100%',
-    paddingHorizontal: 8 * scale,
+    // paddingHorizontal: (width - ITEM_WIDTH) / 2,
+    paddingBottom: 12 * scale,
   },
 
   practiceItem: {
-    width: 80 * scale,
+    width: ITEM_WIDTH,
     alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: -3 * scale
   },
 
-  practiceCenterItem: {
-    width: 80 * scale,
-    alignItems: 'center',
-  },
 
   practiceTextMuted: {
     fontSize: 14 * scale,
-    fontFamily: Fonts.Regular,
-    color: '#666666',
+    color: '#666',
     textAlign: 'center',
     lineHeight: 20 * scale,
   },
@@ -385,33 +365,44 @@ const styles = StyleSheet.create({
   practiceTextActive: {
     fontSize: 18 * scale,
     fontFamily: Fonts.Medium,
-    lineHeight: 24 * scale,
     color: '#000',
     textAlign: 'center',
+    lineHeight: 24 * scale,
+  },
+
+
+  carouselWrapper: {
+    // height: 80 * scale,
+    justifyContent: 'center',
+    alignContent: "center",
+    textAlign: "center",
+    alignItems: "center"
+  },
+
+  centerIndicatorContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   verticalLine: {
     width: 4 * scale,
     height: 40 * scale,
     borderRadius: 24 * scale,
-    marginTop: 22 * scale,
-    // marginBottom: 20 * scale
-
   },
 
   practiceBtn: {
-    // marginBottom: 32 * scale,
-    height: 56 * scale,
+    paddingHorizontal:16*scale,
+    paddingVertical:16*scale,
     borderRadius: 48 * scale,
-    backgroundColor: '#4A2AC9',
+    backgroundColor: '#235DFF',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: 40 * scale,
   },
 
   practiceBtnText: {
-    color: '#FFFFFF',
-    fontSize: 16 * scale,
-    fontFamily: Fonts.Medium, lineHeight: 24 * scale
+    color: '#FFF',
+    fontFamily: Fonts.Medium,
+    lineHeight: 24 * scale,
+    fontSize: 18 * scale
   },
 });
